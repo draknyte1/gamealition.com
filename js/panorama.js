79459ab7c21e1d2f2781494346ba18fce5f58704
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2015-08-14 13:00:13 +0100

    Added support for touch events in panorama

--- js/panorama.js
+++ js/panorama.js
@@ -40,39 +40,53 @@ Panorama.create = function(id, images)
         element.style.backgroundPosition = pano.scroll + "px 0";
     };
 
+    pano.onInputDown = function(e)
+    {
+        pano.lastX = e.touches
+            ? e.touches[0].clientX
+            : e.clientX;
+
+        pano.grabbed = true;
+    };
+
+    /** @param {MouseEvent|TouchEvent} e */
+    pano.onInputUpOrOut = function(e)
+    {
+        if (!e.touches || e.touches.length == 0)
+            pano.grabbed = false;
+    };
+
+    pano.onInputMove = function(e)
+    {
+        if (!pano.grabbed) return;
+
+        var clientX = e.touches
+            ? e.touches[0].clientX
+            : e.clientX;
+
+        var delta = clientX - pano.lastX;
+
+        pano.energy  = delta;
+        pano.scroll += delta;
+        pano.lastX   = clientX;
+        pano.element.style.backgroundPosition = pano.scroll + "px 0";
+    };
+
     var firstImage = images[0];
 
     element.style.backgroundImage    = "url(" + firstImage[0] + ")";
     element.style.backgroundRepeat   = "repeat-x";
     element.style.backgroundPosition = "0px 0px";
 
-    element.onmousedown = function(e)
-    {
-        pano.grabbed = true;
-        pano.lastX   = e.clientX;
-    };
-
-    element.onmouseup = function(e)
-    {
-        pano.grabbed = false;
-    };
-
-    element.onmouseout = function(e)
-    {
-        pano.grabbed = false;
-    };
-
-    element.onmousemove = function(e)
-    {
-        if (!pano.grabbed) return;
-
-        var delta = e.clientX - pano.lastX;
-
-        pano.energy  = delta;
-        pano.scroll += delta;
-        pano.lastX   = e.clientX;
-        pano.element.style.backgroundPosition = pano.scroll + "px 0";
-    };
+    element.addEventListener("mousedown",   pano.onInputDown);
+    element.addEventListener("mouseup",     pano.onInputUpOrOut);
+    element.addEventListener("mouseout",    pano.onInputUpOrOut);
+    element.addEventListener("mousemove",   pano.onInputMove);
+    element.addEventListener("touchstart",  pano.onInputDown);
+    element.addEventListener("touchend",    pano.onInputUpOrOut);
+    element.addEventListener("touchleave",  pano.onInputUpOrOut);
+    element.addEventListener("touchcancel", pano.onInputUpOrOut);
+    element.addEventListener("touchmove",   pano.onInputMove);
 
     console.log("Created panorama", pano);
     Panorama.panos.push(pano);
