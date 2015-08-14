d170569ced5131dda5723550ca765b8d985147fd
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2015-08-14 15:12:48 +0100

    * Added more panoramas
    * Added panorama controls & jquery dependency

--- js/panorama.js
+++ js/panorama.js
@@ -2,22 +2,14 @@ var Panorama = {};
 
 Panorama.panos = [];
 
-Panorama.create = function(id, images)
+Panorama.create = function(id)
 {
     var pano = {};
 
-    /** @type {HTMLElement} */
-    var element = document.getElementById(id);
-
-    if ( !(element instanceof HTMLElement) )
-        throw new Error("Not a valid panorama element ID");
-
-    if (images.length == 0)
-        throw new Error("No images specified");
-
-    pano.element = element;
-    pano.images  = images;
-    pano.current = images[0];
+    pano.jquery  = $(id);
+    pano.element = pano.jquery[0];
+    pano.slides  = Panorama.getSlides(pano.element);
+    pano.current = 0;
     pano.grabbed = false;
     pano.energy  = -1;
     pano.scroll  = 0;
@@ -28,7 +20,7 @@ Panorama.create = function(id, images)
         if (pano.energy < -1 || pano.energy > 1)
             pano.energy *= 0.9;
         else
-            pano.energy = -1;
+            pano.energy  = -1;
 
         if (pano.grabbed) return;
 
@@ -37,7 +29,33 @@ Panorama.create = function(id, images)
         if (pano.scroll < pano.current[1] * -1)
             pano.scroll = 0;
 
-        element.style.backgroundPosition = pano.scroll + "px 0";
+        // Do not use jQuery as this is a fast method
+        pano.element.style.backgroundPosition = pano.scroll + "px 0";
+    };
+
+    pano.update = function()
+    {
+        var jquery  = pano.jquery,
+            current = pano.slides[pano.current];
+
+        jquery.css("background-image",    "url(" + current.src + ")");
+        jquery.css("background-repeat",   "repeat-x");
+        jquery.css("background-position", "0px 0px");
+
+        jquery.find(".title").text(current.title);
+        jquery.find(".subtitle").text(current.subtitle);
+    };
+
+    pano.changeBy = function(delta)
+    {
+        pano.current += delta;
+
+        if      (pano.current < 0)
+            pano.current = pano.slides.length - 1;
+        else if (pano.current >= pano.slides.length)
+            pano.current = 0;
+
+        pano.update();
     };
 
     pano.onInputDown = function(e)
@@ -49,7 +67,6 @@ Panorama.create = function(id, images)
         pano.grabbed = true;
     };
 
-    /** @param {MouseEvent|TouchEvent} e */
     pano.onInputUpOrOut = function(e)
     {
         if (!e.touches || e.touches.length == 0)
@@ -72,26 +89,60 @@ Panorama.create = function(id, images)
         pano.element.style.backgroundPosition = pano.scroll + "px 0";
     };
 
-    var firstImage = images[0];
+    var jquery = pano.jquery;
 
-    element.style.backgroundImage    = "url(" + firstImage[0] + ")";
-    element.style.backgroundRepeat   = "repeat-x";
-    element.style.backgroundPosition = "0px 0px";
+    jquery.bind("mousedown",   pano.onInputDown);
+    jquery.bind("mouseup",     pano.onInputUpOrOut);
+    jquery.bind("mouseout",    pano.onInputUpOrOut);
+    jquery.bind("mousemove",   pano.onInputMove);
+    jquery.bind("touchstart",  pano.onInputDown);
+    jquery.bind("touchend",    pano.onInputUpOrOut);
+    jquery.bind("touchleave",  pano.onInputUpOrOut);
+    jquery.bind("touchcancel", pano.onInputUpOrOut);
+    jquery.bind("touchmove",   pano.onInputMove);
 
-    element.addEventListener("mousedown",   pano.onInputDown);
-    element.addEventListener("mouseup",     pano.onInputUpOrOut);
-    element.addEventListener("mouseout",    pano.onInputUpOrOut);
-    element.addEventListener("mousemove",   pano.onInputMove);
-    element.addEventListener("touchstart",  pano.onInputDown);
-    element.addEventListener("touchend",    pano.onInputUpOrOut);
-    element.addEventListener("touchleave",  pano.onInputUpOrOut);
-    element.addEventListener("touchcancel", pano.onInputUpOrOut);
-    element.addEventListener("touchmove",   pano.onInputMove);
+    jquery.find("button[prev]").click( function() { pano.changeBy(-1); } );
+    jquery.find("button[next]").click( function() { pano.changeBy(1);  } );
 
-    console.log("Created panorama", pano);
+    pano.update();
+
+    console.log("Created panorama with " + pano.slides.length + " slides");
     Panorama.panos.push(pano);
 };
 
+Panorama.getSlides = function(element)
+{
+    var children = element.getElementsByTagName("slide"),
+        slides   = [];
+
+    if (children.length == 0)
+        throw new Error("No slides defined in panorama");
+
+    for (var i = 0; i < children.length; i++)
+    {
+        var slide = {},
+            child = children[i];
+
+        if (!child.attributes['src'])
+            throw new Error("No src specified for slide", child);
+        else
+            slide.src = child.attributes['src'].value;
+
+        if (!child.attributes['width'])
+            throw new Error("No width specified for slide", child);
+        else
+            slide.width = child.attributes['width'].value;
+
+        slide.title    = child.attributes.title
+            ? child.attributes.title.value : "";
+        slide.subtitle = child.attributes.subtitle
+            ? child.attributes.subtitle.value : "";
+
+        slides.push(slide);
+    }
+
+    return slides;
+};
 
 Panorama.loop = function()
 {
